f2aee331baa0086dfed1e0426db181776dee13ed
Will-Bee <78200129+Will-Bee@users.noreply.github.com>
2021-04-11 12:04:38 +0200

    Update PinGuesser.py
    Faster infinite loop (for), optimalized little bit

--- PinGuesser.py
+++ PinGuesser.py
@@ -1,32 +1,36 @@
 from random import randint as r
 from time import time
+from itertools import count as c
 # Imports
 
 # Set Variables
 
-def main(passwd, howManyNumbers):
-    length = 10 ** howManyNumbers
+def main(passwd, length):
+    MaxNum = 10 ** length
     i = 1
     t1 = round(time() * 1000)
-    if passwd >= length:
+    if passwd >= MaxNum:
         print("No way")
         print("Debug: pin =", passwd)
-        print("How many numbers:", howManyNumbers)
+        print("How many numbers:", length)
         exit()
-    while True:
-        x,i = r(0, length), i + 1
-        print(i, "Pokusů   ", howManyNumbers, "Čísel / Čísla", "   ", "Pin:", x)
+    for i in c(start=1):
+        x = r(0, MaxNum)
+        print(i, "Pokusů   ", length, "Počet čísel", "   ", "Pin:", x)
         if x == passwd:
             print("Pin is guessed:", x)
             t2 = round(time() * 1000)
             t = t2 - t1
-            print((t2 - t1) / 1000, "sekund")
+            print(t / 1000, "sekund")
             print(i, "pokusů")
             exit()
+
+
+print(c(start=1))
 
 
 # Main function
 
 
 if __name__ == "__main__":
-    main(497627, 6)
+    main(123, 4)
